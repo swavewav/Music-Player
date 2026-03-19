82d389484b100617fd07be3b25ba8dd49919deb5
Anita <86735948+swavewav@users.noreply.github.com>
2026-03-19 16:57:30 +0100

    Remove commented-out script.js header
    Removed commented-out code for clarity.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
